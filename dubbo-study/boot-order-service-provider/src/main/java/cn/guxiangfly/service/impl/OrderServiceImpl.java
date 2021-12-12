e5eaa552c7b4c6ba9d2a76211bbb90374f3c0df3
guxiang <964999133@qq.com>
2021-12-13 02:00:52 +0800

    feat:修改 pom

---
+++ dubbo-study/boot-order-service-provider/src/main/java/cn/guxiangfly/service/impl/OrderServiceImpl.java
@@ -0,0 +1,29 @@
+package cn.guxiangfly.service.impl;
+
+
+import cn.guxiangfly.bean.UserAddress;
+import cn.guxiangfly.service.OrderService;
+import cn.guxiangfly.service.UserService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Reference
+    UserService userService;
+
+    @Override
+    public List<UserAddress> initOrder(String userId) {
+        System.out.println("用户id："+userId);
+        //1.查询用户的收货地址
+        List<UserAddress> addresses = userService.getUserAddressList(userId);
+        for (UserAddress address : addresses) {
+            System.out.println(address.getUserAddress());
+        }
+        return addresses;
+    }
+}
