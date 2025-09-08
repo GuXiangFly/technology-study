8f851e472f6ac0cdb7279fd8239c44c1d87f211d
guxiang <guxiang@didiglobal.com>
2025-09-08 21:43:32 +0800

    feat: 添加代码

--- java-ai-langchain4j/src/test/java/cn/guxiangfly/ai/langchain4j/AIServiceTest.java
+++ java-ai-langchain4j/src/test/java/cn/guxiangfly/ai/langchain4j/AIServiceTest.java
@@ -1,6 +1,7 @@
 package cn.guxiangfly.ai.langchain4j;
 
 import cn.guxiangfly.assistant.Assistant;
+import cn.guxiangfly.assistant.SeparateChatAssistant;
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
 import dev.langchain4j.service.AiServices;
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,19 @@ public class AIServiceTest {
 //调用service的接口
         String answer = assistant.chat("Hello");
         System.out.println(answer);
+    }
+
+
+    @Autowired
+    private SeparateChatAssistant separateChatAssistant;
+
+    @Test
+    public void testChatMemory5() {
+        String answer1 = separateChatAssistant.chat(1, "我是环环");
+        System.out.println(answer1);
+        String answer2 = separateChatAssistant.chat(1, "我是谁");
+        System.out.println(answer2);
+        String answer3 = separateChatAssistant.chat(2, "我是谁");
+        System.out.println(answer3);
     }
 }
